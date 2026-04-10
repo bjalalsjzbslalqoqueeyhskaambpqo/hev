0a9a03bd77ca7af8aee6a57810fbc1a3280cbe74
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2026-04-10 10:35:53 -0300

    Update BtVpnService.java

--- app/src/main/java/com/blacktunnel/BtVpnService.java
+++ app/src/main/java/com/blacktunnel/BtVpnService.java
@@ -43,13 +43,13 @@ public class BtVpnService extends VpnService {
 
         Builder b = new Builder()
                 .setSession("simple-hev")
-                .setMtu(1500)
+                .setMtu(1300)
                 .addAddress("198.18.0.1", 30)
                 .addAddress("fc00::1", 126)
                 .addRoute("0.0.0.0", 0)
                 .addRoute("::", 0)
-                .addDnsServer("198.18.0.2")
-                .addDnsServer("8.8.8.8");
+                .addDnsServer("8.8.8.8")
+                .addDnsServer("1.1.1.1");
 
         try {
             b.addDisallowedApplication(getPackageName());
@@ -93,15 +93,7 @@ public class BtVpnService extends VpnService {
         hevReady = true;
         SimpleLog.i("HEV listo, iniciando proxy...");
 
-        BtProxy.start(sock -> {
-            // Reintentar protect hasta que el VPN esté listo
-            for (int i = 0; i < 5; i++) {
-                if (protect(sock)) return true;
-                try { Thread.sleep(100); } catch (InterruptedException ignored) {}
-            }
-            SimpleLog.i("WARN: protect() falló 5 veces");
-            return false;
-        });
+        BtProxy.start(sock -> protect(sock));
         SimpleLog.i("Proxy iniciado");
     }
 
@@ -121,20 +113,14 @@ public class BtVpnService extends VpnService {
         File f = new File(getFilesDir(), "hev.yml");
         String yml = "tunnel:\n" +
                      "  name: simple-hev\n" +
-                     "  mtu: 8500\n" +
+                     "  mtu: 1300\n" +
                      "  ipv4: 198.18.0.1\n" +
                      "  ipv6: fc00::1\n" +
                      "socks5:\n" +
                      "  address: 127.0.0.1\n" +
                      "  port: 10809\n" +
                      "  udp: 'udp'\n" +
-                     "  pipeline: true\n" +
-                     "mapdns:\n" +
-                     "  address: 198.18.0.2\n" +
-                     "  port: 53\n" +
-                     "  network: 100.64.0.0\n" +
-                     "  netmask: 255.192.0.0\n" +
-                     "  cache-size: 4096\n" +
+                     "  pipeline: false\n" +
                      "misc:\n" +
                      "  log-level: warn\n";
         try {
