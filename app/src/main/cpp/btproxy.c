1787919b5fc0dbccbc0cef31130abed5f1e0cc2d
bjalalsjzbslalqoqueeyhskaambpqo <cuentaargentina19@gmail.com>
2026-04-11 22:50:06 -0300

    Add deep tunnel diagnostics logs and copy-logs UI action

--- app/src/main/cpp/btproxy.c
+++ app/src/main/cpp/btproxy.c
@@ -167,6 +167,53 @@ static void push_logf(const char *level, const char *fmt, ...) {
     pthread_mutex_unlock(&g_logs_mu);
 }
 
+static const char *frame_name(uint8_t t) {
+    switch (t) {
+        case T_OPEN:  return "OPEN";
+        case T_DATA:  return "DATA";
+        case T_CLOSE: return "CLOSE";
+        case T_PING:  return "PING";
+        case T_PONG:  return "PONG";
+        default:      return "UNK";
+    }
+}
+
+static void log_bytes_preview(const char *tag, const uint8_t *data, size_t len) {
+    if (!data || len == 0) {
+        push_logf("I", "%s len=0", tag);
+        return;
+    }
+    size_t n = len < 24 ? len : 24;
+    char hex[3 * 24 + 1];
+    size_t p = 0;
+    for (size_t i = 0; i < n && p + 3 < sizeof(hex); i++) {
+        p += (size_t)snprintf(hex + p, sizeof(hex) - p, "%02X ", data[i]);
+    }
+    if (p > 0 && hex[p - 1] == ' ') hex[p - 1] = '\0';
+    else hex[p] = '\0';
+    push_logf("I", "%s len=%zu preview[%zu]=%s%s", tag, len, n, hex, (len > n ? " ..." : ""));
+}
+
+static void log_http_block(const char *tag, const char *data, size_t len) {
+    size_t n = len < 512 ? len : 512;
+    char out[1200];
+    size_t p = 0;
+    for (size_t i = 0; i < n && p + 3 < sizeof(out); i++) {
+        char c = data[i];
+        if (c == '\r') {
+            out[p++] = '\\'; out[p++] = 'r';
+        } else if (c == '\n') {
+            out[p++] = '\\'; out[p++] = 'n';
+        } else if ((unsigned char)c < 32 || (unsigned char)c > 126) {
+            out[p++] = '.';
+        } else {
+            out[p++] = c;
+        }
+    }
+    out[p] = '\0';
+    push_logf("I", "%s raw[%zu]=%s%s", tag, len, out, (len > n ? " ..." : ""));
+}
+
 
 static void set_nonblock(int fd) {
     int fl = fcntl(fd, F_GETFL, 0);
@@ -215,6 +262,10 @@ static int tun_send_frame(int tfd, uint8_t type, uint32_t sid, const uint8_t *da
     int niov = (dlen > 0) ? 2 : 1;
     ssize_t total = FRAME_HDR + dlen;
     ssize_t sent  = 0;
+    push_logf("I", "mux TX %s sid=%u len=%u", frame_name(type), sid, dlen);
+    if (dlen > 0 && (type == T_OPEN || type == T_DATA)) {
+        log_bytes_preview("mux TX payload", data, dlen);
+    }
     while (sent < total) {
         ssize_t n = writev(tfd, iov, niov);
         if (n > 0) {
@@ -283,6 +334,7 @@ static int recv_http_headers(int fd, char *out, size_t out_cap, int timeout_sec)
     setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 
     if (used == 0) return -1;
+    log_http_block("http recv", out, used);
     return (int)used;
 }
 
@@ -380,6 +432,7 @@ static int open_tunnel_socket(void) {
     
     char req1[256];
     int  rlen1 = snprintf(req1, sizeof(req1), "GET / HTTP/1.1\r\nHost: %s\r\n\r\n", PROXY_HOST);
+    log_http_block("http send req1", req1, (size_t)rlen1);
     ssize_t ws1 = 0;
     while (ws1 < rlen1) {
         ssize_t n = send(fd, req1 + ws1, rlen1 - ws1, MSG_NOSIGNAL);
@@ -401,6 +454,7 @@ static int open_tunnel_socket(void) {
     int  rlen2 = snprintf(req2, sizeof(req2),
                           "- / HTTP/1.1\r\nHost: %s\r\nUpgrade: websocket\r\nConnection: Upgrade\r\naction: tunnel\r\n\r\n",
                           TUNNEL_HOST);
+    log_http_block("http send req2", req2, (size_t)rlen2);
     ssize_t ws2 = 0;
     while (ws2 < rlen2) {
         ssize_t n = send(fd, req2 + ws2, rlen2 - ws2, MSG_NOSIGNAL);
@@ -479,6 +533,7 @@ static int socks5_server_handshake(int cfd, uint8_t *dest_out, int *dest_len_out
 
     memcpy(dest_out, dest, dlen);
     *dest_len_out = dlen;
+    log_bytes_preview("socks5 dest", dest_out, (size_t)dlen);
     return 0;
 }
 
@@ -524,6 +579,7 @@ static void *conn_thread(void *arg) {
     while (g_running) {
         ssize_t n = recv(cfd, buf, sizeof(buf), 0);
         if (n <= 0) break;
+        push_logf("I", "stream sid=%u client->mux bytes=%zd", sid, n);
         pthread_mutex_lock(&g_streams_mu);
         if (s->used) s->last_active = (int64_t)time(NULL);
         pthread_mutex_unlock(&g_streams_mu);
@@ -549,8 +605,12 @@ static void *tunnel_reader(void *arg) {
         uint32_t sid = ((uint32_t)hdr[1] << 24) | ((uint32_t)hdr[2] << 16)
                      | ((uint32_t)hdr[3] <<  8) |  (uint32_t)hdr[4];
         uint16_t len = ((uint16_t)hdr[5] << 8) | hdr[6];
+        push_logf("I", "mux RX %s sid=%u len=%u", frame_name(ft), sid, len);
         if (len > MAX_PAYLOAD) break;
         if (len > 0 && read_full(tfd, payload, len) < 0) break;
+        if (len > 0 && (ft == T_OPEN || ft == T_DATA)) {
+            log_bytes_preview("mux RX payload", payload, len);
+        }
 
         if (ft == T_DATA) {
             pthread_mutex_lock(&g_streams_mu);
@@ -567,6 +627,7 @@ static void *tunnel_reader(void *arg) {
                     else if (errno == EINTR) continue;
                     else break;
                 }
+                push_logf("I", "stream sid=%u mux->client bytes=%u", sid, len);
             }
         } else if (ft == T_CLOSE) {
             pthread_mutex_lock(&g_streams_mu);
